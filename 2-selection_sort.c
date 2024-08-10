c2ab2ea23a3444f37a268179cde62b2b0d597345
Your Name <you@example.com>
2024-08-10 19:58:33 +0400

    debugged

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,21 +1,21 @@
 #include "sort.h"
 void selection_sort(int *array, size_t size)
 {
-  size_t i = 0, j = 0, min = 0;
-  int tmp = 0;
-
-  for (i = 0; i < size; i++)
-    {
-      min = i;
-      for (j = i + 1; j < size; j++)
-          if (*(array + j) < *(array + min))
-            min = j;
-      if (min != i)
-      {
-        tmp = *(array + min);
-        *(array + min) = *(array + i);
-        *(array + i) = tmp;
-      }
-      print_array(array, size);
-    }
+	size_t i = 0, j = 0, min = 0;
+	int tmp = 0;
+	
+	for (i = 0; i < size; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+			if (*(array + j) < *(array + min))
+				min = j;
+		if (min != i)
+		{
+			tmp = *(array + min);
+			*(array + min) = *(array + i);
+			*(array + i) = tmp;
+		}
+		print_array(array, size);
+	}
 }
